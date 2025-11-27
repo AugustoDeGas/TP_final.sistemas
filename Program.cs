d8d67e249ba065c94484062743302e4f37453582
AugustoDeGas <augustodegasperi2005@gmail.com>
2025-11-27 18:57:45 -0300

    primer commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿namespace TPfinal
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+
+            Console.WriteLine("hola");
+        }
+    }
+}
