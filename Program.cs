1950d9786efa77ca9d5b5dca4f607e5631e2d48c
Fedecampagnolo <federico1239cam@gmail.com>
2025-11-28 17:37:46 -0300

    Inicio del tp

--- Program.cs
+++ Program.cs
@@ -1,22 +1,12 @@
-﻿namespace TPfinal
+﻿using System.Globalization;
+
+namespace TPfinal
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-
-            Console.WriteLine("anda por favor");
-
-            Console.WriteLine("anda por favor");
-            Console.WriteLine("anda por favor");
-
-
-            Console.WriteLine("hola");
-
-
-
-            Console.WriteLine("hola");
+           
         }
     }
 }
